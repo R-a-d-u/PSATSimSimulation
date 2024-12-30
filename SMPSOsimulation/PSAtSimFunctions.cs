a150dc035f3943e0cbc586724d96ae94308da738
CepoiuRaduKosty <radu.cepoiu@gmail.com>
2024-12-30 17:37:54 +0200

    Refactor `PSAtSimFunctions` to accept an executable path and validate it, ensuring better encapsulation and error handling.

--- SMPSOsimulation/PSAtSimFunctions.cs
+++ SMPSOsimulation/PSAtSimFunctions.cs
@@ -6,25 +6,33 @@ namespace SMPSOsimulation
 {
     internal class PSAtSimFunctions
     {
-        private string exePath = @"E:\PSATSIM\psatsim_con.exe";
-        private string configFile = "Test.xml";
-        private string outputFile = "output.xml";
-        private string workingDirectory = @"E:\PSATSIM";
-        private Process currentProcess; // Variable to store the current process
+        private const string exeName = "psatsim_con.exe";
+        private const string configFile = "Test.xml";
+        private const string outputFile = "output.xml";
+        
+        private string exePath;
+        private string workingDirectory;
+        private Process? currentProcess = null; // Variable to store the current process
 
-        // Function to start the simulator with basic settings
-        public void StartSimulator()
+        public PSAtSimFunctions(string exePath)
         {
-            Console.Write(($"{configFile} {outputFile} "));
+            // Check if it's a valid path and ends with .exe
+            if (string.IsNullOrWhiteSpace(exePath) || !exePath.EndsWith(exeName, StringComparison.OrdinalIgnoreCase) ||
+                !File.Exists(exePath))
+            {
+                throw new ArgumentException("This is not a valid path to an psatsim_con.exe file! I'm not dealing with this nonsense!");
+            }
 
-            RunProcess($"{configFile} {outputFile}");
+            this.exePath = exePath;
+            this.workingDirectory = Path.GetDirectoryName(exePath)!;
         }
 
-        public void SimulationOptionsFunction(string command)
+        private void RunSimExe(string? arguments = null)
         {
-            Console.Write(($"{configFile} {outputFile} -{command}"));
-            RunProcess($"{configFile} {outputFile} -{command}");
+            string command = $"{configFile} {outputFile} " + (arguments is null ? "" : $"--{arguments}");
+            RunProcess(command);
         }
+        
         public List<Variation> LoadVariationsFromXml(string filePath)
         {
             var serializer = new XmlSerializer(typeof(PsatSimResults));
